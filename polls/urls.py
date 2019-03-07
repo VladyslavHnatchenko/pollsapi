2a39de7b6979c9e91a9959635ed5cbb3bde51927
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-03-07 16:29:48 +0200

    3/7/19

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,18 @@
+from django.urls import path
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from rest_framework.routers import DefaultRouter
+from .apiviews import PollViewSet
+
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, base_name='polls')
 
 urlpatterns = [
-
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(),
+         name="polls_list"),
 ]
+
+urlpatterns += router.urls
