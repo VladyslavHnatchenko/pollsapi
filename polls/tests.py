0d7a61a871d6798417b6e2da381b75b7963a1d1f
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-03-19 16:26:07 +0200

    3/19/19

--- polls/tests.py
+++ polls/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework.test import APIRequestFactory
-from  rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token
 from polls import apiviews
 from django.contrib.auth import get_user_model
 
 
 class TestPoll(APITestCase):
     def setUp(self):
+        self.client = APIClient()
         self.factory = APIRequestFactory()
         self.view = apiviews.PollViewSet.as_view({'get': 'list'})
         self.uri = '/polls/'
@@ -24,11 +25,29 @@ class TestPoll(APITestCase):
             password='test'
         )
 
+    def test_create(self):
+        self.client.login(username="test", password="test")
+        params = {
+            "question": "How are you?)",
+            "created_by": 1
+        }
+        response = self.client.post(self.uri, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
+                         .format(response.status_code))
+
     def test_list(self):
         request = self.factory.get(self.uri,
                                    HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
         request.user = self.user
         response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
